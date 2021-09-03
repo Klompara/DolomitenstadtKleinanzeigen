ee790469ca9cd19ae443990d65a09842cafbbc30
Kleinlercher Michael <36262189+Klompara@users.noreply.github.com>
2021-09-03 15:17:06 +0200

    Telegram webhook testing

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 })
 
+app.post('/', (req, res) => {
+    console.log(JSON.stringify(req));
+    res.send('Webhook endpoint');
+});
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 })
